7f59f34829494532c35255939fee7806e2301bfd
Jean luc <you@example.com>
2019-03-02 14:42:52 +0100

    synchro

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,24 @@
 <?php
 
-$planckApplicationBootstrap = require(__DIR__.'/../__shared/__planckbootstrap/application-bootstrap.php');
+
+
+
+if(is_file(__DIR__.'/static-vendor/planck/__planckbootstrap/application-bootstrap.php')) {
+    define('PHI_LIB_PATH', __DIR__.'/static-vendor/phi');
+    $planckApplicationBootstrap = require(__DIR__.'/static-vendor/planck/__planckbootstrap/application-bootstrap.php');
+}
+else if(is_file(__DIR__.'/../__shared/static-vendor/planck/__planckbootstrap/application-bootstrap.php')){
+    define('PHI_LIB_PATH', __DIR__.'/../__shared/static-vendor/phi');
+    $planckApplicationBootstrap = require(__DIR__.'/../__shared/static-vendor/planck/__planckbootstrap/application-bootstrap.php');
+}
+else {
+    throw new Exception('Can not load Planck application bootstrap');
+}
+
+
+
+
+
 $planckApplicationBootstrap->getAutoloader()->addNamespace('PlanckeyBlogPublic', __DIR__.'/source/class');
 
 
